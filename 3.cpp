2f8661c713b4281617a39c8285f45f5722a2be96
fadd00 <andhikahutama9@gmail.com>
2024-06-04 15:12:19 +0700

    try dan catch

--- 3.cpp
+++ 3.cpp
@@ -4,5 +4,15 @@
 using namespace std;
 
 int main(){
-    
+    cout << "starting program" << endl;
+    try {
+        array<int, 3> data = {3, 5, 7};
+        cout << data.at(5) << endl;
+    }
+    catch (exception& e) {
+            cout << e.what() << endl;
+        }
+        cout << "ending program" << endl;
+        return 0;
+        
 }
